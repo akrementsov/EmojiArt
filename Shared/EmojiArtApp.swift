4c8c506f7bc9e44d26df66248935ae4f0c1f5f3f
akrementsov <other.akrementsov@gmail.com>
2023-01-29 18:49:09 +0300

    Added project

---
+++ Shared/EmojiArtApp.swift
@@ -0,0 +1,20 @@
+//
+//  EmojiArtApp.swift
+//  EmojiArt
+//
+//  Created by Andrey on 1/17/23.
+//
+
+import SwiftUI
+
+@main
+struct EmojiArtApp: App {
+    @StateObject var paletteStore = PaletteStore(named: "Default")
+    
+    var body: some Scene {
+        DocumentGroup(newDocument: { EmojiArtDocument() }) { config in
+            EmojiArtDocumentView(document: config.document)
+                .environmentObject(paletteStore)
+        }
+    }
+}
---
+++ Shared/UtilityViews.swift
@@ -0,0 +1,92 @@
+//
+//  UtilityViews.swift
+//  EmojiArt
+//
+//  Created by Andrey on 1/17/23.
+//
+
+import SwiftUI
+
+struct OptionalImage: View {
+    var uiImage: UIImage?
+    
+    var body: some View {
+        if uiImage != nil {
+            Image(uiImage: uiImage!)
+        }
+    }
+}
+
+struct AnimatedActionButton: View {
+    var title: String? = nil
+    var systemImage: String? = nil
+    let action: () -> Void
+    
+    var body: some View {
+        Button {
+            withAnimation {
+                action()
+            }
+        } label: {
+            if title != nil && systemImage != nil {
+                Label(title!, systemImage: systemImage!)
+            } else if title != nil {
+                Text(title!)
+            } else if systemImage != nil {
+                Image(systemName: systemImage!)
+            }
+        }
+    }
+}
+
+struct IdentifiableAlert: Identifiable {
+    var id: String
+    var alert: () -> Alert
+    
+    init(id: String, alert: @escaping () -> Alert) {
+        self.id = id
+        self.alert = alert
+    }
+    
+    init(id: String, title: String, message: String) {
+        self.id = id
+        alert = { Alert(title: Text(title), message: Text(message), dismissButton: .default(Text("OK"))) }
+    }
+    
+    init(title: String, message: String) {
+        self.id = title + message
+        alert = { Alert(title: Text(title), message: Text(message), dismissButton: .default(Text("OK"))) }
+    }
+}
+
+extension View {
+    func compactableToolbar<Content>(@ViewBuilder content: () -> Content) -> some View where Content: View {
+        self.toolbar {
+            content().modifier(CompactableIntoContextMenu())
+        }
+    }
+}
+
+struct CompactableIntoContextMenu: ViewModifier {
+    #if os(iOS)
+    @Environment(\.horizontalSizeClass) var horizontalSizeClass
+    var compact: Bool { horizontalSizeClass == .compact }
+    #else
+    let compact = false
+    #endif
+    
+    func body(content: Content) -> some View {
+        if compact {
+            Button {
+                
+            } label: {
+                Image(systemName: "ellipsis.circle")
+            }
+            .contextMenu {
+                content
+            }
+        } else {
+            content
+        }
+    }
+}
